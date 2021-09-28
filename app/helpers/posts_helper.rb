9f3d5f3fccf52e383d430cfb1a8d1f28f1b91756
= <rtyenriques@gmail.com>
2021-09-28 10:49:25 -0700

    Done.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,5 @@
 module PostsHelper
-    def last_updated
-        updated_at.strftime("Last updated %A, %b %e, at %l:%M %p")
+    def last_updated(p)
+        p.updated_at.strftime("Last updated %A, %b %e, at %l:%M %p")
       end
 end
